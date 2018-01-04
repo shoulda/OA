2e51cc5833805d7b4afd3933a15fe4c22a15ab03
Johnny <jiang.li@siemens.com>
2018-01-04 10:57:11 +0800

    增加注释，DayApp类注释

--- src/main/resources/static/js/index/DayApp.js
+++ src/main/resources/static/js/index/DayApp.js
@@ -1,24 +1,21 @@
 /**
- * 获取时间
+ * 工作日类，用于获取五个工作日，以及上周、下周五个工作日的逻辑
  * Created by Johnny on 17/11/19.
  */
 var DayApp = function () {
     this.workDaysPerWeek = 5;
     this.secondsPerDay = 24 * 60 * 60 * 1000;
+    // 用于保存五个工作日的时间对象
     this.currentWorkDaysList = new Array();
 };
 
 
 /**
- *  Sunday 0
- *  Monday 1
- *  Tuesday 1
- *  Wednesday 1
- *  Thursday 1
- *  Friday 1
- *  Saturday 1
- * @param date
- * @returns {Array}
+ *
+ * 根据某个时间，获取它所在的周的周一到周五的五个工作日时间对象
+ *（以便于后续根据时间去请求接口，获取该周的五个工作日的全部项目和任务信息）。
+ * @param date:传入一个Date时间对象。
+ * @returns {Array}:获取date这个时间所在的周 ：周一至周五的五个工作日时间对象。
  */
 DayApp.prototype.getWorkDaysList = function (date) {
     var milliseconds = date.getTime();
@@ -40,8 +37,8 @@ DayApp.prototype.getWorkDaysList = function (date) {
     return this.currentWorkDaysList;
 };
 /**
- *
- * @returns {Array}
+ * 获取下一周的周一到周五的五个工作日时间对象。不需要传入任何参数 ，因为DayApp自身会更新时间的起点。
+ * @returns {Array}：获取下一周五个工作日时间对象。
  */
 DayApp.prototype.getNextWorkDaysList = function () {
     for (var i = 0; i <= this.currentWorkDaysList.length; i++) {
@@ -56,8 +53,8 @@ DayApp.prototype.getNextWorkDaysList = function () {
     return this.currentWorkDaysList;
 };
 /**
- * 设置
- * @returns {Array}
+ * 获取上一周的周一到周五的五个工作日时间对象。不需要传入任何参数 ，因为DayApp自身会更新时间的起点。
+ * @returns {Array}：获取上一周五个工作日时间对象。
  */
 DayApp.prototype.getPreWorkDaysList = function () {
     for (var i = 0; i <= this.currentWorkDaysList.length; i++) {
